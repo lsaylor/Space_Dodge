a20006e6cb533112449667d09c8674ddabf9cbc7
lsaylor <levisaylor0413@gmail.com>
2024-10-02 07:54:51 -0600

    updated but not fixed

--- main_menu.py
+++ main_menu.py
@@ -7,13 +7,17 @@ import time
 WIDTH, HEIGHT = 1000, 800
 MAIN_MENU = pygame.display.set_mode((WIDTH, HEIGHT))
 MAIN_RUN = True
-BACKGROUND_MAINMENU = pygame.image.load("default_button_bg.png")
+BACKGROUND_MAINMENU = pygame.image.load("menu_background.png")
 pygame.display.set_caption("Space Dodge: Main Menu")
-QUIT_BUTTON = button.Button(button.BUTTON_SURFACE, WIDTH/2 - len("QUIT")/2, HEIGHT - button.FONT.get_height() - 200, "QUIT")
 
-def draw(background, quit):
-    MAIN_MENU.blit(BACKGROUND_MAINMENU, (0,0))
-    QUIT_BUTTON.update()
+QUIT_TEXT = "QUIT"
+Q_WIDTH = len(QUIT_TEXT)
+Q_HEIGHT = button.FONT.get_height()
+QUIT_BUTTON = button.Button(button.BUTTON_SURFACE, WIDTH/2-Q_WIDTH/2, HEIGHT-Q_HEIGHT-200, "QUIT")
+
+def draw(main_bg, bg_quit):
+    main_bg.blit(BACKGROUND_MAINMENU, (0,0))
+    bg_quit.update()
 
 
 while MAIN_RUN:
